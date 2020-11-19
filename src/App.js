d287edb646bdb87b26fccf174551c6db62a0a2f2
Josélio Samy <joseliobackup@gmail.com>
2020-11-18 23:11:05 -0300

    portifolio

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import './home.css'
+
+import Header from './Header'
+import Content from './Content'
+
+
+export default () =>
+
+  <div id="body">
+    <div id="page-home">
+    <Header></Header>
+    <Content></Content>
+
+    </div>
+  </div>
---
+++ src/Header.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import logo from './assets/logo.png';
+
+export default ()=>
+
+    <header>
+        <img src={logo} alt="Logotipo" />  
+    </header>
