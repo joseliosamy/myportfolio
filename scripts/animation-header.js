153fdafcaac42c1004cc32df814a59083d7c51f7
Josélio Samy <joseliobackup@gmail.com>
2021-01-06 00:35:29 -0300

    new update

--- scripts/animation-header.js
+++
@@ -1,4 +0,0 @@
-const header = document.getElementsByTagName("header")[0]
-function a_out(){
-    header.classList.toggle("header");
-}
--- scripts/reveal-header.js
+++
@@ -1,9 +0,0 @@
-const btnHamburguer = document.getElementById("hamburguer");
-const headerToHamburguer = document.getElementById("header-hamburguer");
-const headerRevealHamburguer = document.getElementById("header");
-
-function reveal(){
-    if(headerRevealHamburguer){
-        headerRevealHamburguer.classList.toggle("header-hamburguer-on")
-    } 
-}
