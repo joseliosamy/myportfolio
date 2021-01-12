b5879e207bd3aa067d2107953ba6b75f15418db1
Josélio Samy <joseliobackup@gmail.com>
2021-01-11 22:46:25 -0300

    new animations and functions menu

--- scripts/menu.js
+++ scripts/menu.js
@@ -2,11 +2,16 @@ $(function(){
     $('.hamburguer').click(function(){
         var menu = $('.mobile .nav');
         var icon = $('.mobile .icon');
-        var body = $('body');
+        var ul_a = $('.ul a');
         if(menu.is(":hidden") == true){
             menu.slideDown();
             icon.removeClass('fa-bars');
             icon.addClass('fa-times');
+                $(ul_a).click(function(){
+                    menu.slideUp();
+                    icon.removeClass('fa-times');
+                    icon.addClass('fa-bars');
+                })
         }else{
             menu.slideUp();
             icon.removeClass('fa-times');
@@ -14,3 +19,11 @@ $(function(){
         }
     })
 })
+$(function(){
+    $('.ul a').click(function(){
+        var header = $("header");
+        header.slideUp();
+        header.slideDown();
+    })
+})
+
