f8e06e298962af66faa9dec8d07fb921a0dd6f9b
Josélio Samy <joseliobackup@gmail.com>
2021-02-20 02:41:01 -0300

    fav added

--- scripts/scrollAnimation.js
+++ scripts/scrollAnimation.js
@@ -18,7 +18,13 @@ const srTop = ScrollReveal({
 srTop.reveal('.descrip'); // descrip landing
 srTop.reveal('.my-img img'); // my-img landing
 srTop.reveal('.title'); // title about
-srTop.reveal('.intro h3',{delay:200}); // intro about
+srTop.reveal('.about-me .about-header'); // about header
+srTop.reveal('.about-me .about-body .rc',{delay:200}); // about header
+srTop.reveal('.about-me .about-body .text h2',{delay:200}); // about header
+srTop.reveal('.about-me .about-body .text .small',{delay:200}); // about header
+srTop.reveal('.about-me .about-body .text p',{delay:300}); // about header
+srTop.reveal('.about-me .about-body .text .socials',{delay:500}); // about header
+
 srTop.reveal('.intro p',{delay:300}); // intro about
 srTop.reveal('#servicos h1',{delay:100}); // h1 servicos
 srTop.reveal('.type-box',{delay:300}); // type-box servicos
